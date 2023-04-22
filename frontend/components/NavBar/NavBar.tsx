79285331a0a673504dc155b9b8ace01a09ed94f3
anil <anilshrestha0403.as@gmail.com>
2023-04-22 14:20:40 -0700

    nav bar structure created && component Discover created

--- frontend/components/NavBar/NavBar.tsx
+++ frontend/components/NavBar/NavBar.tsx
@@ -1,21 +1,187 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 //import react icons
-import { MdNotifications } from 'react-icons/md';
-import { BsSearch } from 'react-icons/bs';
-import { CgMenuLeft, CgMenuRight } from 'react-icons/cg'
+import { MdNotifications } from "react-icons/md";
+import { BsSearch } from "react-icons/bs";
+import { CgMenuLeft, CgMenuRight } from "react-icons/cg";
+
+//import images
+import images from "../../img";
 
 //internal css module import
-import Style from './NavBar.module.css';
-import { Discover, HelpCenter, Notification, Profile, SideBar } from './index'
+import Style from "./NavBar.module.css";
+
+//import modules
+import { Discover, HelpCenter, Notification, Profile, SideBar } from "./index";
+// import Button from "../Button/Button";
+import { Button } from "../componentindex";
 
 const NavBar = () => {
+  const [discover, setDiscover] = useState(false);
+  const [help, setHelp] = useState(false);
+  const [notification, setNotification] = useState(false);
+  const [profile, setProfile] = useState(false);
+  const [openSideMenu, setOpenSideMenu] = useState(false);
+
+  const openMenu = (e: React.MouseEvent<HTMLParagraphElement>) => {
+    const btnText = (e.target as HTMLElement).innerText;
+    if (btnText === "Discover") {
+      setDiscover(true);
+      setHelp(false);
+      setNotification(false);
+      setProfile(false);
+    } else if (btnText == "Help Center") {
+      setDiscover(false);
+      setHelp(true);
+      setNotification(false);
+      setProfile(false);
+    } else {
+      setDiscover(false);
+      setHelp(false);
+      setNotification(false);
+      setProfile(false);
+    }
+  };
+
+  const openNotification = () => {
+    if (!notification) {
+      setNotification(true);
+      setDiscover(false);
+      setHelp(false);
+      setProfile(false);
+    } else {
+      setNotification(false);
+    }
+  };
+
+  const openProfile = () => {
+    if (!profile) {
+      setNotification(false);
+      setDiscover(false);
+      setHelp(false);
+      setProfile(true);
+    } else {
+      setProfile(false);
+    }
+  };
+  const openSideBar = () => {
+    if (!openSideMenu) {
+      setOpenSideMenu(true);
+      setNotification(false);
+      setDiscover(false);
+      setHelp(false);
+      setProfile(false);
+    } else {
+      setOpenSideMenu(false);
+    }
+  };
+
   return (
-    <div>NavBar</div>
-  )
-}
+    <div className={Style.navbar}>
+      <div className={Style.navbar_container}>
+        {/* // left section */}
+        <div className={Style.navbar_container_left}>
+          <div className={Style.logo}>
+            <Image
+              src={images.logo}
+              alt="NFT Market Place"
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className={Style.navbar_container_left_box_input}>
+            <div className={Style.navbar_container_left_box_input_box}>
+              <input type="text" placeholder="Search NFT" />
+              <BsSearch onClick={() => {}} className={Style.search_con} />
+            </div>
+          </div>
+        </div>
+        {/* // right section */}
+        <div className={Style.navbar_container_right}>
+          <div className={Style.navbar_container_right_discover}>
+            {/* Discover Menu */}
+            <p
+              onClick={(e) => {
+                openMenu(e);
+              }}
+            >
+              Discover
+            </p>
+            {discover && (
+              <div className={Style.navbar_container_right_discover_box}>
+                <Discover />
+              </div>
+            )}
+          </div>
 
+          {/* help center menu */}
+          <div className={Style.navbar_container_right_help}>
+            <p
+              onClick={(e) => {
+                openMenu(e);
+              }}
+            >
+              {" "}
+              Help Center
+            </p>
+            {help && (
+              <div className={Style.navbar_container_right_help_box}>
+                <HelpCenter />
+              </div>
+            )}
+          </div>
 
-export default NavBar
+          {/* notification */}
+          <div className={Style.navbar_container_right_notify}>
+            <MdNotifications
+              className={Style.notify}
+              onClick={() => {
+                openNotification;
+              }}
+            />
+            {notification && <Notification />}
+          </div>
+
+          {/* create button section  */}
+          <div className={Style.navbar_container_right_button}>
+            <Button btnText="Create" />
+          </div>
+
+          {/* User Profile */}
+          <div className={Style.navbar_container_right_profile_box}>
+            <div className={Style.navbar_container_right_profile}>
+              <Image
+                src={images.user1}
+                alt="Profile"
+                width={40}
+                height={40}
+                onClick={() => openProfile}
+                className={Style.navbar_container_right_profile}
+              />
+              {profile && <Profile />}
+            </div>
+          </div>
+
+          {/* menu button for mobile devices only */}
+          <div className={Style.navbar_container_right_button_menuBtn}>
+            <CgMenuRight
+              className={Style.menuIcon}
+              onClick={() => openSideBar}
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* sidebar for mobile devices only */}
+      {openSideMenu && (
+        <div className={Style.SideBar}>
+          <SideBar setOpenSideMenu={setOpenSideMenu} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
