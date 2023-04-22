76dedc471a9341cdf4cc6fb493df0ae0460f36bd
anil <anilshrestha0403.as@gmail.com>
2023-04-22 11:05:55 -0700

    componentes dirs and files setup

---
+++ frontend/components/Footer/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+//internal css module  import
+import Style from "./footer.module.css";
+const footer = () => {
+  return <div>footer</div>;
+};
+
+export default footer;
